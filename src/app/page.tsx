0e8284dee068a4ba0f36cfec2543505414c1bfa6
Filipe Ferandes <flpsan@gmail.com>
2025-01-30 15:55:17 +1100

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-gray-500">
+      <div className="flex flex-col items-center gap-8">
+        <img
+          src="https://bahbq.com.au/wp-content/uploads/2016/01/BahBQ_Logo_150px3.png"
+          alt="WorkLogger Logo"
+          className="w-64 h-64 object-contain"
+        />
+        <a href="/api/auth/signin">
+          <Button size="lg" className="w-40">
+            Login
+          </Button>
+        </a>
+      </div>
     </div>
   );
 }
